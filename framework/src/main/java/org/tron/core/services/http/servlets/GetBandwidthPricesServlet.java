e7b1ac462807583cc98fc6286e0bdc926d68bd7d
GrapeS <jiaoqingsunny@gmail.com>
2026-08-04 16:11:04 +0800

    refactor(http): group servlets into subpackage and flatten services

---
+++ framework/src/main/java/org/tron/core/services/http/servlets/GetBandwidthPricesServlet.java
@@ -0,0 +1,34 @@
+package org.tron.core.services.http.servlets;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.tron.api.GrpcAPI.PricesResponseMessage;
+import org.tron.core.Wallet;
+import org.tron.core.services.http.JsonFormat;
+import org.tron.core.services.http.Util;
+
+@Component
+@Slf4j(topic = "API")
+public class GetBandwidthPricesServlet extends RateLimiterServlet {
+
+  @Autowired
+  private Wallet wallet;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    try {
+      PricesResponseMessage reply = wallet.getBandwidthPrices();
+      response.getWriter().println(reply == null ? "{}" : JsonFormat.printToString(reply));
+    } catch (Exception e) {
+      Util.processError(e, response);
+    }
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    doGet(request, response);
+  }
+}
